be508575e1e252c2e8564d4c602e3a5088703f04
doyeon158 <kdy991201@naver.com>
2025-07-04 12:21:11 +0900

    update

---
+++ Coding/KDY/2025_07_04/43164.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+def solution(tickets):
+    
+    # 티켓 딕셔너리 정의
+    graph = defaultdict(list)
+    
+    for t in tickets:
+        start=t[0]
+        end=t[1]
+        graph[start].append(end)
+        graph[start].sort()
+    print(graph)
+    
+    # 경로 정답
+    answer = []
+    
+    def dfs(now):
+        while graph[now]:
+            dfs(graph[now].pop(0))
+        answer.append(now)
+    
+
+    dfs("ICN")
+
+    return answer[::-1]
