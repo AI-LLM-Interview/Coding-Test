e41d244f453d8c16e69e58550a313d5a4b0ab19c
doyeon158 <kdy991201@naver.com>
2025-07-04 13:15:59 +0900

    update 42860

--- Coding/KDY/2025_07_04/42860.py
+++ Coding/KDY/2025_07_04/42860.py
@@ -1,15 +1,33 @@
+# 조이스틱 문제
+
+# 이동 최솟값 계산
+# JAZZ 라면 'A'AAA -> 'J'AAA -> JAA'Z' -> JA'Z'Z
+# BZAZAAAAAZZAZ 라고 하면 ZZAZ'B'ZAZ AAAAA  4<-B->3
+# 좌우 최소값: 4 + 3 + min(3,4) 또는 한 방향으로 쭉 이동하는 경우 = 13 - 1
+
 def solution(name):
+
     answer = 0
-    length = len(name)
-    min_move = len(name) - 1
+    length = len(name)          # 글자 길이
+    min_move = len(name) - 1    # 한 방향으로만 이동하는 경우
     
     
-    for i, n in enumerate(name):
-        answer += min(ord(n) - ord('A'),ord('Z')-ord(n)+1)
+    for i, text in enumerate(name):    # 인덱스와 문자 순회
+        
+        # 상하 이동 수 계산
+        answer += min(ord(text) - ord('A'),
+                      ord('Z') - ord(text) + 1 )
+
+        # 연속된 A 위치 (i~ii) 계산
         ii = i + 1
         while ii < len(name) and name[ii] =='A':
             ii += 1
+
         front = i
         back = len(name)-ii
-        min_move = min(min_move, front+back+min(front,back))
+
+        # 좌우 이동 수 계산
+        min_move = min(min_move,
+                       front + back + min(front, back))
+        
     return answer + min_move
